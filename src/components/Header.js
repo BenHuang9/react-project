01c3137256ca87ef39d15e117aa240581f91c0f3
Ben Huang <benhuang011@gmail.com>
2022-01-12 16:03:25 -0800

    update logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import NavMain from './NavMain';
-
+import logo from '../images/logo.png'
 function Header() {
     return (
         <header>
-            <h1><Link to='/'>Movie Time</Link></h1>
+            <h1><Link to='/'>
+                <img src={logo} alt="Logo" className='logo'/>
+                </Link></h1>
             <NavMain />
         </header>
     )
