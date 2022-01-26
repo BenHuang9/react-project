a993d45737abbd9e7276004dd3436b18c61e7a36
Ben Huang <benhuang011@gmail.com>
2022-01-25 16:14:03 -0800

    update about logo

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,9 +1,11 @@
+import logo from '../images/tmdbLogo.svg'
+
 function PageAbout() {
     return (
         <section className="about-page">
             <h2>About</h2>
             <p>Movie Time is a website for users to browser movies base on new release, popular, upcoming, and now playing movie. Feel free to click the little heart to add your favourite movie to you favourite movie collection.</p>
-
+            <img src={logo} alt="Logo" className='logo'/>
             <p>This product uses the TMDb API but is not endorsed or certified by TMDb.certified by TMDb.</p>
         </section>
     )
